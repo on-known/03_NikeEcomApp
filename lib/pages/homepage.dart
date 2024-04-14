3184140852e3e9be364779eb8fc924b1a415f78d
on-known <makeharjeet@gmail.com>
2024-04-14 22:08:13 +0530

    "step2"

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,11 +1,43 @@
+import 'package:ecommerce_app_13apr2024/component/bottomnavigationbar.dart';
+import 'package:ecommerce_app_13apr2024/pages/cartpage.dart';
+import 'package:ecommerce_app_13apr2024/pages/shoppage.dart';
 import 'package:flutter/material.dart';
-import 'package:google_nav_bar/google_nav_bar.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void navigationBottomBar(int index) {
+    _selectedIndex = index;
+    setState(() {});
+  }
+
+  final List<Widget> _pages = [
+//shop page
+
+    const ShopPage(),
+
+//cart page
+
+    const CartPage()
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      bottomNavigationBar: MyBottomNavigationBar(
+        onTabChange: (index) {
+          navigationBottomBar(index);
+        },
+      ),
+      body: _pages[_selectedIndex],
+    );
   }
 }
